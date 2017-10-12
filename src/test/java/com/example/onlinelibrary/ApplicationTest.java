f6c32ee9ef1aeb433bc4b20889798ddda6391380
sergey <yakovlev.sergey@outlook.com>
2017-10-12 22:10:01 +0300

    Add test ignore.

--- src/test/java/com/example/onlinelibrary/ApplicationTest.java
+++ src/test/java/com/example/onlinelibrary/ApplicationTest.java
@@ -1,5 +1,6 @@
 package com.example.onlinelibrary;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
